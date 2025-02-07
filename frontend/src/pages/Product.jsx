34aaf0ebb4b34636133b0fd1ede4bd547f433d83
ALANKRIT AGARWAL <itews2216@glbitm.ac.in>
2025-02-07 18:38:13 +0530

    Product Page and Cart Count

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,11 +2,12 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
 
   const {productId} = useParams();
-  const {products, currency} = useContext(ShopContext);
+  const {products, currency, addToCart} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('');
   const [size, setSize] = useState('');
@@ -66,11 +67,11 @@ const Product = () => {
                 ))}
               </div>
             </div>
-            <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+            <button onClick={()=>addToCart(productData._id, size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
             <hr className='mt-8 sm:w-4/5'/>
             <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
               <p>100% Original Product.</p>
-              <p>Cash on Delivey is available on this Product.</p>
+              <p>Cash on Delivery is available on this Product.</p>
               <p>Easy Return and Exchange Policy within 7 Days.</p>
             </div>
           </div>
@@ -87,6 +88,9 @@ const Product = () => {
           <p>Lorem</p>
         </div>
       </div>
+
+      {/* Display Related Products */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
 
     </div>
   ) : <div className='opacity-0'></div>
