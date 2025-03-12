3a959b189ed26d2ca571e43be72a73f71c075a3b
ALANKRIT AGARWAL <itews2216@glbitm.ac.in>
2025-03-12 19:19:18 +0530

    product functionality completed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -46,20 +46,37 @@ const addProducts = async (req, res) => {
 
 //function for list products
 const listProducts = async (req, res) => {
-
+    try {
+        const products = await productModel.find({})
+        res.json({success: true, products})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
 
 }
 
 //function for removing product
 const removeProduct = async (req, res) => {
-
-
+    try {
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success: true, message: "Product removed successfully"})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
 }
 
 //function for single product info
 const singleProduct = async (req, res) => {
-
-
+    try {
+        const {productId} = req.body
+        const product = await productModel.findById(productId)
+        res.json({success: true, product})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
 }
 
 export { addProducts, listProducts, removeProduct, singleProduct }
